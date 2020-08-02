99ff9bd28e4e7310961d57c8aef8cc3359bc7a5a
ryan-ghosh <66140294+ryan-ghosh@users.noreply.github.com>
2020-08-02 11:57:19 -0400

    Update buyModel.py

--- buyModel.py
+++ buyModel.py
@@ -5,6 +5,13 @@ import numpy as np
 import matplotlib as plt
 from sklearn.model_selection import train_test_split
 
+## TODO: move real-time section to a update file/function. In main block, iterate over columns names (stock names), grab specific data using pandas, then process. 
+
+def processData(fileName):
+    df = pd.read_csv(fileName)
+    df['G/B Buy'] = 1 or 0 ## TODO: insert calculation to decide. 
+    ## TODO: insert more changes
+    return df
 
 def buildBuyModel(trainData, testData, trainLabels, testLabels):
     model = models.Sequential([ ## toggle dropout layers to see change in performance. 
@@ -20,3 +27,15 @@ def buildBuyModel(trainData, testData, trainLabels, testLabels):
     model.evaluate(testData, testLabels)
     return model
 
+if __name__ == "__main__":
+    filename = 'GenZ'
+    data = processData(filename)
+    train_data = data.sample(frac=0.8, random_state=0)
+    test_data = data.drop(train_data.index)
+    train_label = train_data.pop('G/B Buy')    ## if this does not work surround in []
+    test_label = test_data.pop('G/B Buy')
+    sellmodel = buildSellModel(train_data, test_data, train_label, test_label)
+    output = sellmodel.predict[## add all current info here]
+    if output > 0.95:
+        print('Buy' + stock_name)
+        print(output + 'confidence')
